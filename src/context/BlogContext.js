e77eb0de636a1c4ec3dc8e67aa5f80bd3fa2bd08
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2021-01-04 11:24:28 -0500

    Configure context to display object

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -4,8 +4,16 @@ const BlogContext = React.createContext();
 
 // component that accepts another component
 export const BlogProvider = ({ children }) => {
-    return <BlogContext.Provider>
-        {children}
-    </BlogContext.Provider>
+    const blogPosts = [
+        {title: 'Blog Post #1'},
+        {title: 'Blog Post #2'}
+    ];
+
+    return (
+        <BlogContext.Provider value={blogPosts}>
+            {children}
+        </BlogContext.Provider>
+    )
 };
 
+export default BlogContext;
