6d08f5cfd39daac00eb730cf54a512c93562c9cb
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2021-01-04 13:13:44 -0500

    Automate context and make it reusable

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,6 +1,4 @@
-import React, { useReducer } from 'react';
-
-const BlogContext = React.createContext();
+import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
     switch(action.type) {
@@ -11,25 +9,14 @@ const blogReducer = (state, action) => {
     }
 };
 
-// component that accepts another component
-export const BlogProvider = ({ children }) => {
-    //blogPosts = state
-    const [blogPosts, dispatch] = useReducer(blogReducer, []);
-    
-    const addBlogPost = () => {
+const addBlogPost = (dispatch) => {
+    return () => {
         dispatch({ type: 'add_blogpost' });
     };
-
-    return (
-        <BlogContext.Provider
-            value={{ 
-                data: blogPosts,
-                addBlogPost
-            }}
-        >
-            {children}
-        </BlogContext.Provider>
-    )
 };
 
-export default BlogContext;
+export const { Context, Provider } = createDataContext(
+    blogReducer, 
+    { addBlogPost }, 
+    []
+);
