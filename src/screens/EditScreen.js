85c212a8c779baf7133ccb00ea8c2dd2d1beae3d
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2021-01-06 13:12:10 -0500

    Move elements to BlogPostForm component

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,6 +1,7 @@
-import React, {useState, useContext} from 'react';
-import {View, Text, StyleSheet, Button, TextInput} from 'react-native';
+import React, { useContext} from 'react';
+import { StyleSheet } from 'react-native';
 import { Context } from '../context/BlogContext';
+import BlogPostForm from '../components/BlogPostForm';
 
 const EditScreen = ({ navigation }) => {
     const { state } = useContext(Context);
@@ -9,18 +10,8 @@ const EditScreen = ({ navigation }) => {
         blogPost => blogPost.id === navigation.getParam('id')
     );
 
-    const [title, setTitle] = useState(blogPost.title);
-    const [content, setContent] = useState(blogPost.content);
 
-
-    return (
-        <View>
-            <Text>Edit Title</Text>
-            <TextInput value={title} onChangeText={(newTitle) => setTitle(newTitle)}/>
-            <Text>Edit Content</Text>
-            <TextInput value={content} onChangeText={(newContent) => setContent(newContent)}/>
-        </View>
-    )
+    return <BlogPostForm />
 }
 
 const styles = StyleSheet.create({
