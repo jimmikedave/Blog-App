c84381b22add5d8b356fec0accfeb80af07f788a
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2021-01-06 12:48:11 -0500

    Link Edit screen to navigation and receive id

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -48,5 +48,5 @@ const deleteBlogPost = (dispatch) => {
 export const { Context, Provider } = createDataContext(
     blogReducer, 
     { addBlogPost, deleteBlogPost }, 
-    []
+    [{title: 'Test Post', content: 'Test Content', id: 1}]
 );
